dcfe45d3448688c8174fbb99b2ba9b6e1729bbaa
Ryan Clark <ryan.clark.j@gmail.com>
2011-10-28 12:25:32 -0700

    Fleshed out sidebar for editing interface

--- static/js/editor/editor.js
+++ static/js/editor/editor.js
@@ -5,7 +5,12 @@ $(document).ready(function() {
 	//	First, make the text of attributes of titles in arrays un-editable	
 	$("ul[eletype='array'] > li > div > div[ele='attribute']").removeAttr("contenteditable");
 	//	Then, move the site information block below the other blocks
-	$("#standard-addition-block").insertBefore("#site-info-block");
+	$("#site-info-block").insertAfter("#map-block");
+	// If the document is in no collections, indicate this
+	collectionList = $("#collections-current-list");
+	if (collectionList.children().length == 0) {
+		collectionList.append("<li class='included-collection nil-collection'>Not published in any collection</li>");
+	}
 });
 
 function writeResource() {
