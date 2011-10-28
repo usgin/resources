570af02aaf70165f14d2ea4edf57cb9c39c000d1
Ryan Clark <ryan.clark.j@gmail.com>
2011-10-28 12:30:50 -0700

    Minor changes to config-example pulling template content together and exposing it as a config param

--- config-example.js
+++ config-example.js
@@ -11,7 +11,7 @@ _dbInfo = {
         dbHarvestName: "harvested",
         dbCollectionName: "collections"
 	},
-	dbVersion: 1.1
+	dbVersion: 1.0
 };
 
 _serverInfo = {
@@ -35,40 +35,42 @@ _searchInfo = {
 	searchUrl: _dbInfo.dbVersion >= 1.1 ? "/_fti/local/" + _dbInfo.dbRepoName + "/_design/indexes/" : "/" + _dbInfo.dbRepoName + "/_fti/_design/indexes/"
 };
 
-_contactTemplate = {
-    Name: "First and Last Name",
-    ContactInformation: {
-	    Phone: "123-456-7890",
-	    email: "example@fake.com",
-	    Address: {
-	    	Street: "1234 Somewhere Pl.",
-	    	City: "Tucson",
-	    	State: "AZ",
-	    	Zip: "12345"
+_storageFormat = function() {
+	this.contactTemplate = {
+	    Name: "First and Last Name",
+	    ContactInformation: {
+		    Phone: "123-456-7890",
+		    email: "example@fake.com",
+		    Address: {
+		    	Street: "1234 Somewhere Pl.",
+		    	City: "Tucson",
+		    	State: "AZ",
+		    	Zip: "12345"
+		    }
 	    }
-    }
+	};
+	this.linkTemplate = {
+		Type: "Describe how the link is to be used (download, wms, random service, etc.)",
+		URL: "http://fake.server.com/path/to/your/resource"
+	};
+	this.metadataTemplate = {
+		Title: "The title of the resource being described",
+		Description: "A description of what the resource being described is about",
+		Authors: [ this.contactTemplate ],
+	    PublicationDate: "Please use a format like this: 2011-10-11T14:30",
+	    Keywords: ["List", "Some", "Keywords"],
+	    GeographicExtent: {
+	    	NorthBound: 45.0,
+	    	SouthBound: 30.0,
+	    	EastBound: -112.0,
+	    	WestBound: -109.0
+	    },
+	    Distributors: [ this.contactTemplate ],
+	    Links: [ this.linkTemplate ]
+	};
 };
 
-_linkTemplate = {
-	Type: "Describe how the link is to be used (download, wms, random service, etc.)",
-	URL: "http://fake.server.com/path/to/your/resource"
-};
-
-_metadataTemplate = {
-	Title: "The title of the resource being described",
-	Description: "A description of what the resource being described is about",
-	Authors: [ _contactTemplate ],
-    PublicationDate: "Please use a format like this: 2011-10-11T14:30",
-    Keywords: ["List", "Some", "Keywords"],
-    GeographicExtent: {
-    	NorthBound: 45.0,
-    	SouthBound: 30.0,
-    	EastBound: -112.0,
-    	WestBound: -109.0
-    },
-    Distributors: [ _contactTemplate ],
-    Links: [ _linkTemplate ]
-};
+_storage = new _storageFormat();
 
 _defaultContext = {
 	mainLogo: _serverInfo.serverLogoLocation,
@@ -77,12 +79,13 @@ _defaultContext = {
 	orgName: _organizationInfo.orgName,
 	orgUrl: _organizationInfo.orgUrl,
 	orgYear: _organizationInfo.orgYear,
-	contactTemplate: JSON.stringify(_contactTemplate),
-	linkTemplate: JSON.stringify(_linkTemplate),
-	metadataTemplate: JSON.stringify(_metadataTemplate)
+	contactTemplate: JSON.stringify(_storage.contactTemplate),
+	linkTemplate: JSON.stringify(_storage.linkTemplate),
+	metadataTemplate: JSON.stringify(_storage.metadataTemplate)
 };
 
 exports.dbInfo = _dbInfo;
 exports.serverInfo = _serverInfo;
 exports.searchInfo = _searchInfo;
 exports.defaultContext = _defaultContext;
+exports.storageFormat = _storage;
