73e48152addcf6b11dfd1fe10384666674f4ae95
Ryan Clark <ryan.clark.j@gmail.com>
2011-10-28 11:47:09 -0700

    Added the rest of the tools to the editing interface

--- static/js/editor/toolbar-template.js
+++ static/js/editor/toolbar-template.js
@@ -3,6 +3,9 @@ var jade = require("jade");
 var controlList = [];
 
 controlList.push("ul.control-tools");
+controlList.push("\t"+ "li.add-obj-button(onclick=addObjFunction)");
+controlList.push("\t"+ "li.add-arr-button(onclick=addArrFunction)");
+controlList.push("\t"+ "li.add-val-button(onclick=addValFunction)");
 controlList.push("\t" + "li.minimize-button(onclick=minimizeFunction)");
 controlList.push("\t" + "li.delete-button(onclick=deleteFunction)");
 
@@ -13,10 +16,29 @@ function controlToolbar(id, canDelete, canMinimize) {
 	context = {
 		canDelete: canDelete,
 		id: id,
+		addObjFunction: "addObject('" + id + "')",
+		addArrFunction: "addArray('" + id + "')",
+		addValFunction: "addValue('" + id + "')",
 		minimizeFunction: "minimize('#" + id + "')",
 		deleteFunction: "deleteObj('#" + id + "')"
 	};
 	return controlFn(context);
+}
+
+function addObject(parentId) {
+	thisId = getId();
+	addHtml(objAsHtml("NewProperty", "object", thisId), parentId, true);
+	addHtml(controlToolbar(thisId, true, true), thisId + "-header", true);
+}
+
+function addArray(parentId) {
+	thisId = getId();
+	addHtml(objAsHtml("NewProperty", "array", thisId), parentId, true);
+	addHtml(controlToolbar(thisId, true, true), thisId + "-header", true);
+}
+
+function addValue(parentId) {
+	addHtml(valueAsHtml("Add a new value", "NewProperty"), parentId, true);
 }
 
 function minimize(id) {
