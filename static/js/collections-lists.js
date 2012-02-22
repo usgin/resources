eb40afc0c4ba7d752de890c229e1134c43e9aa83
Genhan Chen <genhan.chen@azgs.az.gov>
2012-02-22 15:03:48 -0700

    Content browser inside each container

--- static/js/collections-lists.js
+++ static/js/collections-lists.js
@@ -3,86 +3,118 @@ $(document).ready(function() {
 });
 
 function showRecords(collectionId) {
-	var contentEle = $("#" + collectionId + "-record-list");
+	var titleTriangleEle = $("#" + collectionId + "-container > .block-title > span");
+	
+	attachRecords(collectionId, collectionId, titleTriangleEle);	
+}
+
+///Attach the results to a specific container element
+////Parameters:
+////searchTerm - collection ID used for search keyword
+////collectionEleId - the selected collection element id; string type
+////titleTriangleEle - the expand triangle symbol element
+function attachRecords(searchTerm, collectionEleId, titleTriangleEle){
+	var contentEle = $("#" + collectionEleId);
 	
 	if (contentEle.children().length > 0) {
 ///**********Close the collection container*********************		
-		expandSymbolChange(false, collectionId); ///Change the expand symbol
-		clearCollectionContent(contentEle, collectionId);
+		expandSymbolChange(false, titleTriangleEle); ///Change the expand symbol
+		clearCollectionContent(collectionEleId);
 		
 	} else {
 ///*********Expand the collection container*********************
-		expandSymbolChange(true, collectionId); ///Change the expand symbol
-		addCollectionContent(contentEle, collectionId);		
+		expandSymbolChange(true, titleTriangleEle); ///Change the expand symbol
+		addCollectionContent(searchTerm, collectionEleId);		
 
-	}
+	}	
 }
 
-function clearCollectionContent(contentEle, collectionId) {
+function clearCollectionContent(collectionEleId) {
+	var contentEle = $("#" + collectionEleId);
 	contentEle.empty();
 }
 
-function addCollectionContent(contentEle, collectionId) {
+function addCollectionContent(searchTerm, collectionEleId) {
+	var contentEle = $("#" + collectionEleId);
+	
 	/// Search records in the repository database with a specific collection ID
 	searchObj = {
 		index : "collection",
-		terms : collectionId
+		terms : searchTerm
+	};
+	
+	/// Search childern collection with a specific parent collection ID
+	searchChildrenCollectionObj = {
+		index : "parent", ///Attribute name in the fulltext property in collections/_design/indexes, which is defined in indexes.js
+		terms : searchTerm ///The search term in the parent property
 	};
 	
 	$.post("/search/", searchObj, function(response) {
 		for(r in response.rows) { 
-			contentEle.append(getHtmlRecord(response.rows[r]));
+			contentEle.append(getHtmlRecord(response.rows[r], collectionEleId));
 		}
-	});
-
-	/// Search childern collection with a specific parent collection ID
-	searchChildrenCollectionObj = {
-		index : "parent", ///Attribute name in the fulltext property in collections/_design/indexes, which is defined in indexes.js
-		terms : collectionId ///The parent collection ID
-	};
 		
-	$.post("/collection-search/", searchChildrenCollectionObj, function(response) {
-		for(r in response.rows) { 
-			contentEle.append(getHtmlCollection(response.rows[r]));
-		}
+		/// Set collection search after the record search is completed
+		$.post("/collection-search/", searchChildrenCollectionObj, function(response) {
+			for(r in response.rows) { 
+				contentEle.append(getHtmlCollection(response.rows[r], collectionEleId));
+			}
+		});
 	});
 }
-
-function getHtmlCollection(collection) {
-	var collectionContainerId = collection.id + "-container";
-	
-	var html = "<li id=" + collectionContainerId + " class='record-container collection-item'>";
-	html += "<span class='ui-icon ui-icon-triangle-1-e collection-list-expand'></span>";
+////////////////////////////////////////////////////////////////////////////////////////////////
+///Get collection with children
+///Parameters:
+////collection - the collection search result
+////collectionEleId - the collection content element id
+function getHtmlCollection(collection, collectionEleId) {	
+	var collectionId = collection.id + "-" + collectionEleId;
+	var html = "<li id='" + collectionId + "-container' class='record-container collection-item'>";
+	html += "<span class='ui-icon ui-icon-triangle-1-e collection-list-expand'";
+	html += " onclick='collectionExpand(&#39;" + collectionId + "&#39;)'"; /// Use escape symbol to identify the quote symbol pair
+	html += "></span>";
 	html += "<a>";
 	html += collection.doc.Title || "No Title Was Given";
 	html += "</a>";
+	html += "<div id='" + collectionId + "' class='record-list record-inner-list'></div>"; /// This is the collection content element
 	html += "</li>";
 	
 	return html;	
 }
 
-function getHtmlRecord(record) {
-	var recordContainerId = record.id + "-container";
+function collectionExpand(collectionEleId){
+	var titleTriangleEle = $("#" + collectionEleId + "-container > span");
+	
+	attachRecords(collectionEleId.split("-")[0], collectionEleId, titleTriangleEle);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+///Get single record without children
+///Parameters:
+////record - the record search result
+////collectionEleId - the id of the collection content element which contains this record
+function getHtmlRecord(record, collectionEleId) {
+	
+	var recordContainerId = record.id + "-" + collectionEleId;
 	var recordLink = "/resource/" + record.id + "/html";
 	
-	var html = "<li id=" + recordContainerId + " class='record-container'>";
-	html += "<a href=" + recordLink + ">";
+	var html = "<li id='" + recordContainerId + "' class='record-container'>";
+	html += "<a href=" + recordLink + " class='record-item-link'>";
 	html += record.doc.Title || "No Title Was Given";
 	html += "</a>";
 	html += "</li>";
 	
 	return html;
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////////////
 ///Change the triangle symbol in title bar
-function expandSymbolChange(isExpand, collectionId){
-	var titleTriangleEle = $("#" + collectionId + "-container > .block-title > span");
+function expandSymbolChange(isExpand, triangleEle){
 	if(isExpand){
-		titleTriangleEle.removeClass("ui-icon-triangle-1-e");
-		titleTriangleEle.addClass("ui-icon-triangle-1-s");		
+		triangleEle.removeClass("ui-icon-triangle-1-e");
+		triangleEle.addClass("ui-icon-triangle-1-s");		
 	}else{
-		titleTriangleEle.removeClass("ui-icon-triangle-1-s");
-		titleTriangleEle.addClass("ui-icon-triangle-1-e");
+		triangleEle.removeClass("ui-icon-triangle-1-s");
+		triangleEle.addClass("ui-icon-triangle-1-e");
 	}
 }
 
